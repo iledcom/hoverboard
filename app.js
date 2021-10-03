bb3ad3a2d08104754bdd4b2abc614785b7c33642
Konstantyn Korenev <korenev@iled.com.ua>
2021-10-03 22:28:03 +0300

    home work 4 end[C

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#e74c3c', '#8e44ad', '#3498ab', '#e67e22', '#e73cd7', '#2ecc71', '#d7e73c']
+const SQUARES_NUMBER = 900
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
